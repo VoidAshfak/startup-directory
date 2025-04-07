008c56dbd738b7748cb6c8c1a96563dc48302dd5
VoidAshfak <1703103@student.ruet.ac.bd>
2025-04-07 20:10:13 +0600

    sign in with github OAuth, setup project, setup styles, implement navbar

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { auth } from "../../../auth";
+
+export default async function Home() {
+    const session = await auth();
+    return (
+        <div>
+            <h1>Home</h1>
+            <p>{JSON.stringify(session)}</p>
+        </div>
+    );
+}
